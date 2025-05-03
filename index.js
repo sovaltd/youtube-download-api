edaa0f1750549f689b422eed86d9405b968e3f87
Vahid Mahdavi <sovaltd@gmail.com>
2025-05-03 23:41:01 +0100

    new version

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const ytdl = require('@distube/ytdl-core');
-
+const ytdl = require("@distube/ytdl-core");
 const cors = require("cors");
-
+const fs = require("fs");
+const Cookiefile = require("cookiefile");
 const app = express();
 app.use(cors());
-
+const cookiefile = new Cookiefile.CookieMap("cookies.txt");
+const cookies = cookiefile.toRequestHeader().replace("Cookie: ", "");
 app.get("/", (req, res) => {
     const ping = new Date();
     ping.setHours(ping.getHours() - 3);
@@ -36,6 +37,7 @@ app.get("/info", async (req, res) => {
     }
 });
 
+
 app.get("/mp3", async (req, res) => {
     const { url } = req.query;
 
@@ -43,21 +45,33 @@ app.get("/mp3", async (req, res) => {
         const isValid = ytdl.validateURL(url);
 
         if (isValid) {
-            const videoName = (await ytdl.getInfo(url)).videoDetails.title;
+            const info = await ytdl.getInfo(url);
+            const videoName = info.videoDetails.title;
 
             res.header(
                 "Content-Disposition",
                 `attachment; filename="${videoName}.mp3"`
             );
-            res.header("Content-type", "audio/mpeg3");
+            res.header("Content-Type", "audio/mpeg");
 
-            ytdl(url, { quality: "highestaudio", format: "mp3" }).pipe(res);
+            ytdl(url, {
+                quality: "highestaudio",
+                requestOptions: {
+                    headers: {
+                        Cookie: cookies,
+                    },
+                },
+            }).pipe(res);
         } else {
-            res.status(400).send("Invalid url");
+            res.status(400).send("Invalid URL");
         }
     } else {
         res.status(400).send("Invalid query");
     }
+});
+
+app.listen(process.env.PORT || 3500, () => {
+    console.log("Server is running");
 });
 
 app.get("/mp4", async (req, res) => {
